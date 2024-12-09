caa9444abfcc6bc4d0bceff4f91b0af4175482e5
Suthar Darshan <suthardarshan2782@gmail.com>
2024-12-09 12:25:35 +0530

    Add registered user

--- src/app.js
+++ src/app.js
@@ -23,6 +23,16 @@ app.use(cookieParser())
 app.use(express.static("public"))
 
 
+// routes import
+import userRouter from './routes/user.routes.js'
+
+
+
+// routes declaration
+app.use("/api/v1/users", userRouter)
+
 export {
     app
 }
+
+// http://localhost:8000/api/v1/users/register
